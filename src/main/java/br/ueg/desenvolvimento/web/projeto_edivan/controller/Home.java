496576184e5953b1404b3fa9c9df1a1d375373bb
Edivan1000 <edivanprofissional10@gmail.com>
2025-02-18 20:58:43 -0300

    Add files via upload

---
+++ src/main/java/br/ueg/desenvolvimento/web/projeto_edivan/controller/Home.java
@@ -0,0 +1,21 @@
+package br.ueg.desenvolvimento.web.projeto_edivan.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Home {
+
+        @GetMapping("/")
+        public String getHome(Model model) {
+            List alunos = new ArrayList<>();
+            alunos.add(Map.of("nome", "João", "email", "joao@localhost"));
+            model.addAttribute("alunos", alunos);
+            return "home"; // refers to classpath resource /templates/home.html
+}
+}
