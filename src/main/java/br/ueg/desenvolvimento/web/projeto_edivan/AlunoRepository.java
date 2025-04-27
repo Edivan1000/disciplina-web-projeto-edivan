13343547b026cec7deb4fc259e5530b57ee11930
Edivan1000 <edivanprofissional10@gmail.com>
2025-04-27 19:50:00 -0300

    Delete src directory

--- src/main/java/br/ueg/desenvolvimento/web/projeto_edivan/AlunoRepository.java
+++
@@ -1,6 +0,0 @@
-package br.ueg.desenvolvimento.web.projeto_edivan;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlunoRepository extends JpaRepository<Aluno, Integer>{
-}
